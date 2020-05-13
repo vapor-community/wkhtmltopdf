fa155d2ffa678d78145af5e69bfff0a0cab48be2
Graham Burgsma <graham.burgsma@mindsea.com>
2020-05-12 20:15:40 -0400

    Updated Document initializers with defaults instead of optionals

--- Sources/wkhtmltopdf/Page.swift
+++ Sources/wkhtmltopdf/Page.swift
@@ -1,10 +1,15 @@
 import Foundation
+import NIO
 
 public struct Page {
     let content: Data
 
     public init(_ content: Data) {
         self.content = content
+    }
+
+    public init(_ content: ByteBuffer) {
+        self.content = Data(content.readableBytesView)
     }
 
     public init(_ content: String) {
